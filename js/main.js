d89525f419578df06d4fec160c9736a1e9bf00fa
Harold Espinoza <haroldesptru@gmail.com>
2019-06-04 12:03:30 -0600

    Añadido la segmentación de los componentes principales del sitio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-const routes = {
-  '/': 'index route'
-};
+const routes = {};
 
-const navbarItems = document.querySelectorAll('.navbar--item');
+const navbarItems = document.querySelectorAll('.navbar--item'),
+      rootComponent = document.getElementById('page-content');
 
 if(navbarItems){
   navbarItems.forEach(item => {
+
+    // getting all components templates of site
+    let _route = item.getAttribute('page-target'),
+        _componentName = item.getAttribute('page-component').toLowerCase(),
+        _component = document.getElementById(`component__${_componentName}`);
+
+    if(_route && _component){
+      routes[_route] = _component.innerHTML;
+      _component.remove();
+    }else{
+      console.error(`Navbar Item empty: ${item.textContent}`);
+    }
+
+
+    // Adding events to navba--item's
     item.addEventListener('click', e => {
       const prev = document.querySelector('.navbar--item.active'),
             next = e.target;
@@ -15,7 +29,9 @@ if(navbarItems){
       e.preventDefault();
 
       /* Router code for change pages */
-      let pathName = next.getAttribute('page-target');
+      let pathName = next.getAttribute('page-target'); // get route target
+
+      rootComponent.innerHTML = routes[pathName]; // loading component to main container
 
       window.history.pushState(
         {},
@@ -25,3 +41,10 @@ if(navbarItems){
     });
   });
 }
+
+(function(){
+  console.log('auto, '+window.location.href);
+  if([window.location.origin, window.location.origin+'/'].includes(window.location.href)){
+    rootComponent.innerHTML = routes['/'];
+  }
+})();
